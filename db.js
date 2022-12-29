674a8aebca28a8b9b45f5de6b34a3c9efd90fdff
Sito8943 <sito8943@gmail.com>
2022-12-28 19:27:15 -0500

    0.4.3

--- db.js
+++ db.js
@@ -1,19 +1,3 @@
-// @ts-check
 
-const { initializeApp } = require("firebase/app");
-const { getFirestore } = require("firebase/firestore");
-
-// Initialize Firebase
-var app;
-
-// Initialize Cloud Firestore and get a reference to the service
-var db;
-
-const initDatabase = (
-  /** @type {{apiKey: String; authDomain: String; projectId: String; storageBucket: String; messagingSenderId: String; appId: String}} */ config
-) => {
-  app = initializeApp(config);
-  db = getFirestore(app);
-};
 
 module.exports = { db, initDatabase };
--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // @ts-check
-const { db, initDatabase } = require("./db");
+
+const { initializeApp } = require("firebase/app");
+const { getFirestore } = require("firebase/firestore");
+
+// Initialize Firebase
+var app;
+
+// Initialize Cloud Firestore and get a reference to the service
+var db;
+
+const initDatabase = (
+  /** @type {{apiKey: String; authDomain: String; projectId: String; storageBucket: String; messagingSenderId: String; appId: String}} */ config
+) => {
+  app = initializeApp(config);
+  db = getFirestore(app);
+};
+
 const {
   collection,
   doc,
